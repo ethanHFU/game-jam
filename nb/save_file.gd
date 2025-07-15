27ac77f60a22680278068f869c63cea3eb9f7612
nico <nicoberndt234@gmail.com>
2025-07-15 23:48:23 +0200

    save file resource

---
+++ nb/save_file.gd
@@ -0,0 +1,21 @@
+class_name SaveFile
+extends Resource
+
+
+### Game States ###
+@export var level_unlocked = {
+	"Acheron":		true,
+	"Phlegeton":	false,
+	"Kokytos":		false,
+}
+
+# collected buffs
+@export var boat_speed: float
+@export var boat_life: float
+####################
+
+
+### Settings ###
+@export var music_volume: float
+@export var effects_volume: float
+################
