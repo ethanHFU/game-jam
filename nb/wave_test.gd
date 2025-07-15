238aff611ca9484f5f850632d1bdd2ea0d65bfdc
nico <nicoberndt234@gmail.com>
2025-07-15 14:28:43 +0200

    better wave shader

--- nb/wave_test.gd
+++ nb/wave_test.gd
@@ -1,19 +1,16 @@
 extends Node3D
 
-@export var ampl_curve: Curve
-
 var _clicked: bool = false
 var _counter: float = 0.0
 
 func _physics_process(delta):
 	var toggle: bool = true
 	if toggle == _clicked:
-		_counter = lerp(_counter, 1.0, delta)
-		RenderingServer.global_shader_parameter_set("ampl_decrement", ampl_curve.sample(_counter)*1.5)
-		if _counter <= 0.0:
+		_counter = lerp(_counter, 20.0, delta*.75)
+		RenderingServer.global_shader_parameter_set("expand_wave", _counter)
+		if _counter >= 19.0:
 			_clicked = false
-			_counter = RenderingServer.global_shader_parameter_get("ampl_decrement")
-		
+			_counter = 0.0
 
 func _on_static_body_3d_input_event(camera, event, event_position, normal, shape_idx):
 	if Input.is_action_just_pressed("click"):
