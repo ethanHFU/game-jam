d30323ad79cd0a4e248f70018d60414a3cd4d389
tiram-isu <tiram5@zenker-schmitz.net>
2025-07-18 00:22:06 +0200

    changed branch

--- Scripts/GameStateSystems/EventBus.gd
+++ Scripts/GameStateSystems/EventBus.gd
@@ -4,15 +4,16 @@ extends Node
 ### save and load ###
 signal save_to_disc
 signal load_from_disc
-#####################
 
 ### load scenes ###
 signal load_scene(scene: String)
-###################
 
+### audio stuff ###
+signal play_sound(stream: String)
 
 ### level stuff ###
 signal take_damage(amount: float)
 signal repair_boat(amount: float)
 signal make_invincible()
 signal boost_boat()
+signal trigger_level_end()
